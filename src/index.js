f9b55a391a6d1900688ee5ee89e0e5fe47dcd691
ajinkyabhuse3798 <ajinkyacbhuse@gmail.com>
2024-05-02 00:59:51 +0100

    Revert "Merge pull request #6 from ajinkyabhuse3798/landingpage"
    This reverts commit cb397c967f4d977bdb78782b3c96f2fd09b853a0, reversing
    changes made to 32765af64f8e1c36ba958eda6ab9cbcbcd9ad5d7.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createRoot } from "react-dom/client";
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from "react-router-dom";
-import createGlobalStyle from "./global";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-createGlobalStyle();
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
